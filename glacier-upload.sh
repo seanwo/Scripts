1e0ac535e8dd543ac209d3fe21e1f94028746ab0
Sean Wohlgemuth <seanwo@gmail.com>
2022-04-03 10:09:54 -0500

    cleanup glacierupload scripts

--- glacier-upload.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -jar ~/.glacieruploader/glacieruploader.jar --partsize 1073741824 --multipartupload $1
---
+++ glacierupload.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar ~/.glacieruploader/glacieruploader.jar "$@"
