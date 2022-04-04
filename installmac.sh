f60231c370ccee2fe36b4dfcc20e12e4cef8d691
Sean Wohlgemuth <seanwo@gmail.com>
2022-04-03 19:57:02 -0500

    updates to deal with environment variables

--- installmac.sh
+++ installmac.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
+# Cache Sudo
+
+sudo echo
+
 # Homebrew
 
 echo Homebrew
 which -s brew
 if [[ $? != 0 ]] ; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    if [[ $(uname -m) == 'arm64' ]]; then
+    	echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.bash_profile    
+    else
+    	echo 'eval "$(/usr/local/bin/brew shellenv)"' >> ~/.bash_profile
+    fi
     echo Homebrew installed.
-else
-    brew update
-    echo Homebrew updated.
 fi
-
 if [[ $(uname -m) == 'arm64' ]]; then
-	brewbin="/opt/homebrew"
+	eval "$(/opt/homebrew/bin/brew shellenv)"
 else
-	brewbin="/usr/local/bin"
+	eval "$(/usr/local/bin/brew shellenv)"
 fi
+brew update
+echo Homebrew updated.
 
 read -p "Press enter to continue"
 
@@ -60,11 +67,11 @@ declare -a mas_apps=(
 
 echo AppStore Applications
 for val in ${mas_apps[@]}; do
-	if ! $brewbin/mas list | grep -q "$val"; then
-		$brewbin/mas install $val
+	if ! $HOMEBREW_PREFIX/bin/mas list | grep -q "$val"; then
+		$HOMEBREW_PREFIX/bin/mas install $val
 	fi
 done
-$brewbin/mas upgrade
+$HOMEBREW_PREFIX/bin/mas upgrade
 
 read -p "Press enter to continue"
 
@@ -78,7 +85,7 @@ declare -a brew_taps=(
 
 echo Homebrew Taps
 for val in ${brew_taps[@]}; do
-	$brewbin/brew tap $val
+	brew tap $val
 done
 
 read -p "Press enter to continue"
@@ -90,7 +97,6 @@ declare -a brew_apps=(
 "awscli"
 "ethereum"
 "gnupg"
-"jenv"
 "node"
 "telnet"
 "wget"
@@ -99,9 +105,17 @@ declare -a brew_apps=(
 echo Homebrew Applications
 for val in ${brew_apps[@]}; do
 	if ! brew ls --versions $val > /dev/null; then
-		$brewbin/brew install $val
+		brew install $val
 	fi
 done
+
+if ! brew ls --versions jenv > /dev/null; then
+	brew install jenv
+	echo 'export PATH="$HOME/.jenv/bin:$PATH"' >> ~/.bash_profile   
+	echo 'eval "$(jenv init -)"' >> ~/.bash_profile
+	export PATH="$HOME/.jenv/bin:$PATH"
+	eval "$(jenv init -)"
+fi
 
 read -p "Press enter to continue"
 
@@ -149,10 +163,10 @@ declare -a cask_apps=(
 echo Homebrew Casks
 for val in ${cask_apps[@]}; do
 	if ! brew ls --versions --cask $val > /dev/null; then
-		$brewbin/brew install --cask $val
+		brew install --cask $val
 	fi
 done
 
-$brewbin/brew upgrade
+brew upgrade
 
 read -p "Press enter to continue"
