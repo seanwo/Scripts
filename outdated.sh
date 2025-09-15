de68052f412899f2bdc6e7677a22672e18d3d645
Sean Wohlgemuth <seanwo@gmail.com>
2025-09-15 18:13:19 -0500

    remove direct cast update

--- outdated.sh
+++ outdated.sh
@@ -2,6 +2,5 @@
 source ~/Scripts/local_casks.sh
 brew update --quiet
 brew outdated --greedy --verbose | grep "(latest) != latest" -v
-brew outdated --cask ~/Scripts/filezilla.rb
 mas outdated
 /usr/sbin/softwareupdate --list 
--- upgrade.sh
+++ upgrade.sh
@@ -3,5 +3,4 @@ source ~/Scripts/local_casks.sh
 brew update --quiet
 brew upgrade
 brew cu -a -y
-brew upgrade --cask ~/Scripts/filezilla.rb
 mas upgrade
